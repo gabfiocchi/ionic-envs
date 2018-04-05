c20148569d8a6034d04e79c17e032ad28fca8177
Gabriel Fiocchi <gab@blowapp.com.ar>
2018-04-04 21:36:55 -0300

    Add message of enviroment running.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,10 +2,16 @@ const path = require('path');
 const useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 const tsconfig = require('../tsconfig.json');
 
+const chalk = require('chalk');
+const log = console.log;
+
 const fs = require('fs');
 const args = require('minimist')(process.argv);
 
 let aliases = {};
+
+log(`${chalk.yellow('Running on enviroment:')} ${chalk.green(args.env || 'dev')}`);
+
 let pathKeyArray = Object.keys(tsconfig.compilerOptions.paths);
 pathKeyArray.forEach(currentPath => {
   let correctPath = currentPath.replace("/*", "");
@@ -27,13 +33,16 @@ useDefaultConfig.dev.resolve.alias['@env'] = path.resolve(environmentPath(args.e
 
 function environmentPath(env) {
   let fileBasePath = './src/environments/environment';
-  let filePath = `${fileBasePath}.${env}.ts`;
-  console.log(path.resolve(filePath))
-  return path.resolve(filePath);
+  let filePath = `${fileBasePath}.${env || 'dev'}.ts`;
+
+  if (!fs.existsSync(filePath)) {
+    log(chalk.red(`There was an error the file does not exist: ${filePath}`));
+    process.exit();
+  } else {
+    return path.resolve(filePath);
+  }
 }
 
 module.exports = function () {
   return useDefaultConfig;
 };
-
-
